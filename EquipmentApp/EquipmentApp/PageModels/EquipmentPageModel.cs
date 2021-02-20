30a497975d4316217785d0197ac1993853a63499
Oyewole Oni <ooni@eminenttechnology.com>
2021-02-20 21:26:55 +0100

    Adding Title and Toolbar

--- EquipmentApp/EquipmentApp/PageModels/EquipmentPageModel.cs
+++ EquipmentApp/EquipmentApp/PageModels/EquipmentPageModel.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace EquipmentApp.PageModels
 {
@@ -14,12 +15,19 @@ namespace EquipmentApp.PageModels
         private IRestServices _restServices;
 
         public ObservableCollection<Equipment> Equipments { get; set; }
+        public Command AddEquipmentCommand { get; set; }
 
         public EquipmentPageModel(IRestServices restServices)
         {
             _restServices = restServices;
             Equipments = new ObservableCollection<Equipment>();
             LoadEquipments();
+            AddEquipmentCommand = new Command(addEquipment);
+        }
+
+        private void addEquipment(object obj)
+        {
+            throw new NotImplementedException();
         }
 
         private async void LoadEquipments()
