fa7fff9f285434cde2969fb81309743668b85cc6
Oyewole Oni <ooni@eminenttechnology.com>
2021-02-20 20:46:20 +0100

    Adding Services

--- EquipmentApp/EquipmentApp/Interfaces/IRestServices.cs
+++ EquipmentApp/EquipmentApp/Interfaces/IRestServices.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using EquipmentApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EquipmentApp.Interfaces
 {
     interface IRestServices
     {
-
+        Task<List<Equipment>> GetEquipments();
+        Task<Equipment> GetEquipment(string id);
+        Task<List<Equipment>> SearchEquipment(string name);
+        Task<bool> PostEquipment(Equipment equipment);
     }
 }
---
+++ EquipmentApp/EquipmentApp/Services/RestServices.cs
@@ -0,0 +1,40 @@
+﻿using EquipmentApp.Interfaces;
+using EquipmentApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentApp.Services
+{
+    public class RestServices : IRestServices
+    {
+        public async Task<Equipment> GetEquipment(string id)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"http://etestapi.test.eminenttechnology.com/api/Equipment/{id}");
+            return JsonConvert.DeserializeObject<Equipment>(response);
+        }
+
+        public  async Task<List<Equipment>> GetEquipments()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("http://etestapi.test.eminenttechnology.com/api/Equipment");
+            return JsonConvert.DeserializeObject<List<Equipment>>(response);
+        }
+
+        public Task<bool> PostEquipment(Equipment equipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Equipment>> SearchEquipment(string name)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"http://etestapi.test.eminenttechnology.com/api/Equipment/search?name={name}&status=1&type=1");
+            return JsonConvert.DeserializeObject<List<Equipment>>(response);
+        }
+    }
+}
