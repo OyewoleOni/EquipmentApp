ca6e904f88267102f7c809545457e89008edd1e2
Oyewole Oni <ooni@eminenttechnology.com>
2021-02-20 21:12:06 +0100

    Adding the Equipment Detail Page and PageModel

---
+++ EquipmentApp/EquipmentApp/Pages/EquipmentDetailModel.cs
@@ -0,0 +1,32 @@
+﻿using EquipmentApp.Models;
+using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EquipmentApp.Pages
+{
+   public class EquipmentDetailModel : FreshBasePageModel
+    {
+        public Equipment Equipment { get; set; }
+
+        public Command EditCommand { get; set; }
+
+        public EquipmentDetailModel()
+        {
+            EditCommand = new Command(editEquipment);
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            Equipment = (Equipment)initData;
+        }
+
+        private void editEquipment(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
