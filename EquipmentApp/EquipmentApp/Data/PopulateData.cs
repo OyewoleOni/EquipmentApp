2cdcb6deaf51529a71838b50f995f026295387d6
Oyewole Oni <ooni@eminenttechnology.com>
2021-02-23 17:36:56 +0100

    Addition to Populate Data locally

---
+++ EquipmentApp/EquipmentApp/Data/PopulateData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentApp.Data
+{
+    public static class PopulateData
+    {
+        public static List<EquipmentApp.Models.EquipmentType> GetTypes()
+        {
+            List<EquipmentApp.Models.EquipmentType> types = new List<EquipmentApp.Models.EquipmentType>()
+            {
+                new Models.EquipmentType()
+                {
+                    Id = 1,
+                    TypeName = "Indoor"
+                },
+                 new Models.EquipmentType()
+                {
+                    Id = 2,
+                    TypeName = "Outdoor"
+                }
+            };
+            return types;
+            
+        }
+    }
+}
---
+++ EquipmentApp/EquipmentApp/ViewModel/EquipmentPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentApp.ViewModel
+{
+    public class EquipmentPost
+    {
+        public string Name { get; set; }
+        public int Status { get; set; }
+        public string StatusName { get; set; }
+        public int Type { get; set; }
+        public int Quantity { get; set; }
+        public string TypeName { get; set; }
+    }
+}
